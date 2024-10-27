8d8497e083ba32ba73b2c6563b522d8609aa6ddd
devnaveedsarwar <learnwithnaveedsarwar@gmail.com>
2024-10-27 12:20:35 +0500

    feat: added login form

--- app/login/page.js
+++ app/login/page.js
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <div>Login
+        <form>
+            <input type="text" placeholder="username" />
+            <input type="password" placeholder="password" />
+            <button type="submit">Login</button>
+        </form>
+    </div>
   )
 }
